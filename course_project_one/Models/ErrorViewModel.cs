256ba51316f5d58b416b6a43a5474ff9c962e081
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-05-04 21:22:11 +0300

    new_prj

--- course_project_one/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace course_project_one.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
