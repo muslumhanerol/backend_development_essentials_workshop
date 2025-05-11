c961ff0c452865175cd04e0d9bb4998856249b4c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-05-11 23:08:59 +0300

    project_formapp_start

---
+++ project_second_formsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace project_second_formsApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
