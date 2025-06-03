c46e8ba9b598a99963e7dbbde9854e0740693b12
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-06-03 23:44:29 +0300

    Update DataContext.cs

--- project_third_efcoreApp/Data/DataContext.cs
+++ project_third_efcoreApp/Data/DataContext.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace project_third_efcoreApp.Data
 {
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
 
 }
