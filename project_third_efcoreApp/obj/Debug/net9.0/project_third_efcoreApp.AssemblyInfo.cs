452c1b0d6ff1389e242bf2db07b9c8a580867273
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2025-07-08 23:04:43 +0300

    db

--- project_third_efcoreApp/obj/Debug/net9.0/project_third_efcoreApp.AssemblyInfo.cs
+++ project_third_efcoreApp/obj/Debug/net9.0/project_third_efcoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_third_efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6de229034d945ef299b640a9b66cd7a4f795fda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b81f56cedd63d283fdf6a4249bc531d7dba67d")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_third_efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_third_efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
